9828b9f222c2305aa47717ae6b80499ae2c69a3e
aofleejay <aofleejay@hotmail.com>
2018-07-29 01:29:12 +0700

    Create question model

---
+++ Quizzler/Question.swift
@@ -0,0 +1,19 @@
+//
+//  Question.swift
+//  Quizzler
+//
+//  Created by Kunapot Pairat on 29/7/2561 BE.
+//  Copyright © 2561 London App Brewery. All rights reserved.
+//
+
+import Foundation
+
+class Question {
+    let questionText: String
+    let answer: Bool
+    
+    init(text: String, correctAnswer: Bool) {
+        questionText = text
+        answer = correctAnswer
+    }
+}
